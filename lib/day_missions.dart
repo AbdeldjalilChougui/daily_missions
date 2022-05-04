dfc67e1a09acb8bd0b0d06685429f28363f4d2c5
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-05-04 19:52:18 +0100

    refactor day mission

--- lib/day_missions.dart
+++ lib/day_missions.dart
@@ -4,82 +4,11 @@ import 'package:dailymissions/avan_incon.dart';
 import 'package:dailymissions/main.dart';
 import 'package:dailymissions/mission_detail.dart';
 import 'package:dailymissions/missions.dart';
+import 'package:dailymissions/models/mission_model.dart';
 import 'package:dailymissions/sql_helper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
-
-
-List<String> list = [
-  "الورد اليومي",
-  "Flutter",
-  "حفظ ربع من القرآن",
-  "دراسة",
-  "Python",
-  "Java",
-  "Sport",
-  "English",
-];
-
-List<String> list2 = [
-  "الورد اليومي",
-  "Graphic Design",
-  "حفظ ربع من القرآن",
-  "دراسة",
-  "Python",
-  "Web",
-  "Sport",
-  "English",
-];
-
-List<String> list_1 = [
-  "الورد اليومي",
-  "Flutter",
-  "دراسة",
-  "Python",
-  "Java",
-  "Sport",
-  "English",
-];
-
-List<String> list2_1 = [
-  "الورد اليومي",
-  "Graphic Design",
-  "دراسة",
-  "Python",
-  "Web",
-  "Sport",
-  "English",
-];
-
-List<Icon> iconList = [
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-];
-
-List<Icon> iconList2 = [
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
-];
-
-List<bool> isPressedList = [
-  false,false,false,false,false,false,false,false
-];
-
-List<bool> isPressedList2 = [
-  false,false,false,false,false,false,false
-];
 
 
 class DayMissions extends StatefulWidget {
