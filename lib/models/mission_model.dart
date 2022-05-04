09ba1e78e2e46efa2386104f5e050431b19893d7
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-05-04 19:51:59 +0100

    push mission model

---
+++ lib/models/mission_model.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+List<String> list = [
+  "الورد اليومي",
+  "Flutter",
+  "حفظ ربع من القرآن",
+  "دراسة",
+  "Python",
+  "Java",
+  "Sport",
+  "English",
+];
+
+List<String> list2 = [
+  "الورد اليومي",
+  "Graphic Design",
+  "حفظ ربع من القرآن",
+  "دراسة",
+  "Python",
+  "Web",
+  "Sport",
+  "English",
+];
+
+List<String> list_1 = [
+  "الورد اليومي",
+  "Flutter",
+  "دراسة",
+  "Python",
+  "Java",
+  "Sport",
+  "English",
+];
+
+List<String> list2_1 = [
+  "الورد اليومي",
+  "Graphic Design",
+  "دراسة",
+  "Python",
+  "Web",
+  "Sport",
+  "English",
+];
+
+List<Icon> iconList = [
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+];
+
+List<Icon> iconList2 = [
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+  Icon(Icons.check_box_outline_blank,size: 30,color: Colors.purple,),
+];
+
+List<bool> isPressedList = [
+  false,false,false,false,false,false,false,false
+];
+
+List<bool> isPressedList2 = [
+  false,false,false,false,false,false,false
+];
